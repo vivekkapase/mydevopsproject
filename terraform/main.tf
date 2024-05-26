c6f0c0b3667c79f19cb799caccb64402d32cdea0
Vivek <iamvivek77@gmail.com>
2024-05-26 11:56:54 +0530

    tf script update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
+
 
 provider "aws" {
   region  = "us-west-2"  # Choose your desired region
