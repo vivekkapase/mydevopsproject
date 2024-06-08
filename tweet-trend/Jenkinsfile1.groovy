a9ab3f3b447ca60374a5271798652035fa6d0e94
Vivek <iamvivek77@gmail.com>
2024-06-08 14:39:56 +0530

    Jenkinsfile added to main branch

---
+++ tweet-trend/Jenkinsfile1.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent {
+        node{
+            label 'build-slave'
+        }
+    }
+environment {
+    PATH = "/opt/apache-maven-3.9.7/bin/:$PATH"
+}
+
+    stages {
+        stage('Build code') {
+            steps {
+                sh '''
+                cd tweet-trend
+                mvn clean deploy
+                '''
+            }
+        }
+    }
+}
