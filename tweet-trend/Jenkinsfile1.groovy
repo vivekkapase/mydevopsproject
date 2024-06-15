0abdf9f02d50c57356b5ded1d5c3a01cbf4e01f8
Vivek <iamvivek77@gmail.com>
2024-06-15 19:21:17 +0530

    added Jfrog publish stage

--- tweet-trend/Jenkinsfile1.groovy
+++ tweet-trend/Jenkinsfile1.groovy
@@ -12,8 +12,10 @@ environment {
         stage('Build code') {
             steps {
                 sh '''
+                echo "Code is getting built....."
                 cd tweet-trend
                 mvn clean deploy
+                echo "Code build completed. Proceeding to publish the artifact."
                 '''
             
             }
@@ -33,6 +35,31 @@ environment {
         
 
     //   }
-    
+        def registry = 'https://vivekdevops.jfrog.io/'
+        stage("Jar Artifact Publish over Jfrog") {
+         steps {
+            script {
+                    echo '<--------------- Jar Publish Started --------------->'
+                     def server = Artifactory.newServer url:registry+"/artifactory" ,  credentialsId:"vivek-jfrog-token"
+                     def properties = "buildid=${env.BUILD_ID},commitid=${GIT_COMMIT}";
+                     def uploadSpec = """{
+                          "files": [
+                            {
+                              "pattern": "tweet-trend/jarstaging/(*)",
+                              "target": "vivek-maven-libs-release-local/{1}",
+                              "flat": "false",
+                              "props" : "${properties}",
+                              "exclusions": [ "*.sha1", "*.md5"]
+                            }
+                         ]
+                     }"""
+                     def buildInfo = server.upload(uploadSpec)
+                     buildInfo.env.collect()
+                     server.publishBuildInfo(buildInfo)
+                     echo '<--------------- Jar Publish Ended --------------->'  
+            
+            }
+        }   
+      }   
     }
 }
